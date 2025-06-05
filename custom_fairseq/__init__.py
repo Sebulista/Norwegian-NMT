dfeeb010f474d239333fa679a4aef49738c76c9a
ec-jonassf <ec-jonassf@login-3.fox.ad.fp.educloud.no>
2025-06-05 13:01:25 +0200

    removed r-drop

--- custom_fairseq/__init__.py
+++ custom_fairseq/__init__.py
@@ -1,4 +1,2 @@
 from . import transformer_deep
 from . import epsilon_task
-from .translation_rdrop_src import rdrop_translations
-from .translation_rdrop_src.loss import reg_label_smooth_cross_entropy
